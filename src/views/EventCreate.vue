6ab5b8aa4c2a7976e97a0fce58ed6502fd09f6ee
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2022-12-15 11:30:42 +0000

    l3

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -62,7 +62,7 @@
 
 <script>
 import { v4 as uuidv4 } from 'uuid'
-
+import EventService from '@/services/EventService.js'
 export default {
   data () {
     return {
@@ -91,7 +91,13 @@ export default {
     onSubmit() {
       this.event.id = uuidv4()
       this.event.organizer = this.$store.state.user
-      console.log("Event:", this.event)
+      EventService.postEvent(this.event)
+      .then(() => {
+
+      })
+      .catch(err => {
+        console.log("Error:", err)
+      })
     }
   }
 }
